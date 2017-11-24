de4f1bc194bd18a2f5e3f6b219a689e4a4bdaa0d
Jay Lyerly <jayl@sonicbunny.org>
2017-11-24 09:37:37 -0500

    Add Demo1 example.

---
+++ Demo1/Demo1/Demo1/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by Jay Lyerly on 11/24/17.
+//  Copyright © 2017 Oak City Labs. All rights reserved.
+//
+
+import AVKit
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var avPlayerView: AVPlayerView!
+}
+
